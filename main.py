e9de9d5b069653dc91ab22fd247d067af45a0d54
DblNbKA <104922491+DblNbKA@users.noreply.github.com>
2023-10-20 17:11:38 +0300

    main.py

--- main.py
+++ main.py
@@ -5,13 +5,12 @@ try:
         idlist = ["1", "2", "3", "4"] # example
         idel = 4 # example
 except telebot.apihelper.ApiException as e:
-    if "Forbidden" in e:
-        index_to_del = idlist.index(str(idel)) # we found id which blocked your bot
-        if 0 <= index_to_del < len(idlist): # checking the id exists
-            del idlist[index_to_del] # delet id from list
-        with open("text.txt", "w") as file: 
-            for el in idlist: 
-                file.writelines(f'{el}\n') # rewriting your file
+    index_to_del = idlist.index(str(idel)) # we found id which blocked your bot
+    if 0 <= index_to_del < len(idlist): # checking the id exists
+        del idlist[index_to_del] # delet id from list
+    with open("text.txt", "w") as file: 
+        for el in idlist: 
+            file.writelines(f'{el}\n') # rewriting your file
 
 # was
 #1
